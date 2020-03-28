b5e3211a94700f17f455f07364d4b92e193b3415
Luciana Silva Araujo Costa <luciaana.sn573@gmail.com>
2020-03-28 19:21:52 -0300

    comitando a criacao da visualizacao dos detalhes do filme

--- src/app/movie/view-movie/view-movie.component.ts
+++ src/app/movie/view-movie/view-movie.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmesDetalhes } from 'src/app/module/movieDetails.model';
+import { Genero } from 'src/app/module/genre.model';
+import { Service } from 'src/app/service/service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-movie',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewMovieComponent implements OnInit {
 
-  constructor() { }
+  
+  public idFilme:Number;
+  public filme: FilmesDetalhes;
+  private error: any;
+  public nome:string;
+  public descricao:string;
+  public dataDeLancamento:string;
+  public generos: Array<Genero>;
+  public duracao:Number;
+  public avaliacao:Number;
+  public foto: string;
+
+  constructor( private service: Service, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.idFilme =  this.activatedRoute.snapshot.params['id'];
+    this.getterFilm(this.idFilme);
+  }
+
+  //pegar os dados da api para lista filmes por mais recentes e por genero
+  getterFilm(id: Number) {
+    this.service.getDetalhes(id).subscribe(
+      (data: FilmesDetalhes) =>{
+        this.filme = data;
+        this.nome=this.filme.original_title;
+        this.descricao = this.filme.overview;;
+        this.dataDeLancamento = this.filme.release_date;
+        this.duracao = this.filme.runtime;
+        this.generos= this.filme.genres;
+        this.avaliacao = this.filme.vote_average;
+        this.foto = 'https://image.tmdb.org/t/p/original'+this.filme.poster_path;
+        console.log(this.generos);
+      },
+      (error: any) =>{
+        this.error=error;
+        console.error('ERROR: ', error);
+      }
+    );    
+
   }
 
 }
