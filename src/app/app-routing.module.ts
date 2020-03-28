17660a2c939c9d2d646f6dd55df71d5f3ea429ce
Luciana Silva Araujo Costa <luciaana.sn573@gmail.com>
2020-03-28 19:24:06 -0300

    comitando a criacao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,55 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GenresListComponent } from './movie/genres-list/genres-list.component';
+import { SearchListComponent } from './movie/search-list/search-list.component';
+import { ViewMovieComponent } from './movie/view-movie/view-movie.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'fimes', pathMatch: 'full' },
+  {
+    path: 'filmes',
+    children: [
+      {
+        path: '',
+        component: GenresListComponent
+      },
+      {
+        path: ':genero',
+        component: GenresListComponent
+      },
+      
+    ]
+  },
+  {
+    path: 'buscar',
+    children: [
+      {
+        path: '',
+        component: SearchListComponent
+      },
+      {
+        path: ':buscar',
+        component: SearchListComponent
+      },
+      
+    ]
+  },
+  {
+    path: 'detalhes',
+    children: [
+      {
+        path: '',
+        component: ViewMovieComponent
+      },
+      {
+        path: ':id',
+        component: ViewMovieComponent
+      },
+      
+    ]
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
