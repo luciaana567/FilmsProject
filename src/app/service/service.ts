286ac48dc0f25e76369928334a2c6082c9759f51
Luciana Silva Araujo Costa <luciaana.sn573@gmail.com>
2020-03-29 20:34:45 -0300

    comitando modificacao nos servicos

--- src/app/service/service.ts
+++ src/app/service/service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
 
 
 @Injectable({
@@ -9,13 +10,15 @@ import {HttpClient} from '@angular/common/http';
 export class Service {
 
   private baseURL= 'https://api.themoviedb.org/3/discover/movie';
-  private now;
+  private ano;
+  private dia;
+  private mes;
   private languageEn = '&language=en-US';
   private languagePortuguese = '&language=pt-BR';
   private numPage = '&page=';
   private generoId;
   private generoNome;
-
+  
 
   constructor(private http: HttpClient) {}
 
@@ -24,27 +27,46 @@ export class Service {
     return '?api_key=66e45b061138e65ddc9f6027cc3ec7c6';
   }
 
+  //pegar a data
+  private getDate(): string{
+    this.ano = new  Date().getFullYear();
+    this.dia = new Date().getUTCDate();
+    this.mes = new Date().getMonth()+1;
+    let data;
+    if(this.mes<10 ){
+       data = this.ano+'-0'+this.mes+'-'+this.dia;
+    }
+    if(this.dia<10){
+      data =  this.ano+'-'+this.mes+'-0'+this.dia;
+    }
+    if(this.dia<10 && this.mes<10){
+      data =  this.ano+'-0'+this.mes+'-0'+this.dia;
+    }
+    return data;
+  }
+
   //lista de filmes da api por genero
   public getListFilms(genero:any,page:Number): Observable<any> {
-      this.now = new Date().getFullYear();
       let url;
       let genres ='&with_genres='
-      
+      let data = this.getDate();
+
         if(this.generoId==='Todos'){
-          url =this.baseURL+this.getKey()+this.languageEn+'&sort_by=release_date.desc&include_adult=true&include_video=false'+this.numPage+page+'&year='+this.now; 
+          url =this.baseURL+this.getKey()+this.languageEn+'&sort_by=release_date.desc&include_adult=true&include_video=false'+this.numPage+page+'&primary_release_date.lte='+data; 
         }else if(this.generoId==='Populares'){
-          url =this.baseURL+this.getKey()+this.languageEn+'&sort_by=popularity.desc&include_adult=true&include_video=false'+this.numPage+page+'&year='+this.now; 
+          url =this.baseURL+this.getKey()+this.languageEn+'&sort_by=popularity.desc&include_adult=true&include_video=false'+this.numPage+page+'&primary_release_date.lte='+data; 
         }else {
-          url =this.baseURL+this.getKey()+this.languageEn+'&sort_by=release_date.desc&include_adult=true&include_video=false'+this.numPage+page+'&year='+this.now+genres+genero; 
+          url =this.baseURL+this.getKey()+this.languageEn+'&sort_by=release_date.desc&include_adult=true&include_video=false'+this.numPage+page+'&primary_release_date.lte='+data+genres+genero; 
         } 
-    
+    console.log(data)
       return this.http.get(url);
   }
 
   //lista de filmes da pesquisa da api
   public getListSearch(nomeFilme:any,page:Number): Observable<any>{
-    
+
     const url='https://api.themoviedb.org/3/search/movie'+this.getKey()+'&query='+nomeFilme+this.numPage+page+'&include_adult=true';
+    console.log(url)
     return this.http.get(url);
 
   }
