d7fe3888c5d3de581cd975d372c734fcca7d11c7
Luciana Silva Araujo Costa <luciaana.sn573@gmail.com>
2020-03-31 13:28:57 -0300

    definindo rotas, criando menu,estilo e funcao para redirecionar aos links

--- src/app/body/nav/nav.component.ts
+++ src/app/body/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaGenero } from 'src/app/module/genre.model';
+import { Router } from '@angular/router';
+import { Service } from 'src/app/service/service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  public listGeneros = new ListaGenero();
+  private error: any;
+
+  public generoId;
+  public generoName;
+
+  constructor( private service: Service,private router: Router) { }
 
   ngOnInit(): void {
+    this.generoId='Todos';
+    this.generoName='Todos';
+    
   }
+
+
+    //escolher o genero da página
+    setGenero(id: any, name:string){
+      this.generoId = id;
+      this.service.setGenero(id,name);
+      this.router.navigateByUrl('/filmes/' +id+'/'+1);
+    }
 
 }
